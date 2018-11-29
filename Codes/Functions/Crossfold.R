dd4e0b56b13bd3c7b04f575a6eb3965a6a3f334d
KRRichard <34747890+KRRichard@users.noreply.github.com>
2018-11-29 14:48:15 -0500

    Added Sliding window functionality

--- Codes/Functions/Crossfold.R
+++ Codes/Functions/Crossfold.R
@@ -1,45 +1,16 @@
-load("//climsidfs07/refeng/1 Ref. Engineering (SH, Scroll & IPD)/13) Analytics/Small Projects/Stocks/Data/Window_Results.RDATA")
-
-
-DF = Window_Results %>%
-  filter(Stock == "AMZN") %>%
-  select(Date, everything(), -Stock) %>%
-  na.omit() %>%
-  arrange(Date)
- # rownames(DF) = NULL
- 
-# DF = DF2[1:300,]
-
-start_width = 125
-step = 1
 
 Benchmark = function(DF, start_width = 125, step = 1, expand = T){
-
+  
   library(tidyverse)
   library(zoo)
   library(xts)
-  library(forecast)
-  
-  
-  # DF=DF %>%
-  #   select(Date, Total_Quantity) %>%
-  #   arrange(Date)
-  # 
-  # Year = DF[(nrow(DF)-11):nrow(DF),]
-  # year(Year$Date) = year(Year$Date) + 1
-  # Year$Total_Quantity = 0
-  # DF = rbind(DF, Year)
-  # 
-  # DF = as.xts(x = DF$Total_Quantity,order.by = DF$Date)
-  
-
   
   
   PRED_Func = function(TRAIN,TEST,H){
     suppressWarnings(expr)
     Model = glm(Buy ~ .-Date,
-               TRAIN,
-               family = 'binomial')
+                TRAIN,
+                family = 'binomial')
     Pred = predict(Model,
                    newdata = TEST, 
                    type = "response")
@@ -52,28 +23,42 @@ Benchmark = function(DF, start_width = 125, step = 1, expand = T){
   }
   
   Walk_FWD_CV = function(DF,start_width,step,expand = T){
-print("Test")
+    print("Test")
     Train = DF[1:start_width,]
     CV_Results = list() 
-    for(i in 1:floor((nrow(DF)-start_width)/step -12)){
-      Test = DF[(nrow(Train)+1):(nrow(Train)+step),]
-    
-      # TRAIN = Train
-      # TEST=Test
-
-            
-      CV_Results[[i]] = PRED_Func(Train,Test,step)
-      Train = rbind(Train,Test)
-    
-      # if(!expand){
-      #   DF = DF[(step+1):length(DF),]
-      #   Train = Train[(step+1):length(Train),]
-      # }
+    if(expand){
+      for(i in 1:floor((nrow(DF)-start_width)/step -12)){
+        Test = DF[(nrow(Train)+1):(nrow(Train)+step),]
+        
+        # TRAIN = Train
+        # TEST=Test
+        
+        
+        CV_Results[[i]] = PRED_Func(Train,Test,step)
+        Train = rbind(Train,Test)
+      }
     }
+    if(!expand){
+      Iteration = 1
+      for(i in 1:floor((nrow(DF)-start_width)/step -12)){
+        
+        Test = DF[(nrow(Train)+Iteration):(nrow(Train)+Iteration),]
+        
+        # TRAIN = Train
+        # TEST=Test
+        
+        CV_Results[[i]] = PRED_Func(Train,Test,step)
+        
+        
+        Train = DF[(Iteration+step):(nrow(Train)+Iteration),]
+        Iteration = Iteration + 1
+      }
+    }
+    
     CV_Results = plyr::ldply(CV_Results,data.frame)
     return(CV_Results)
   }
-
+  
   CV = Walk_FWD_CV(DF = DF,
                    start_width = start_width,
                    step = step,
@@ -82,14 +67,27 @@ print("Test")
   return(CV)
 }
 
+load("//climsidfs07/refeng/1 Ref. Engineering (SH, Scroll & IPD)/13) Analytics/Small Projects/Stocks/Data/Window_Results.RDATA")
 
+library(tidyverse)
+DF = Window_Results %>%
+  filter(Stock == "AMZN") %>%
+  select(Date, everything(), -Stock) %>%
+  na.omit() %>%
+  arrange(Date)
 
+### expand = T - expanding train set
+#          = F - sliding window            
+### start width - for expanding window this is the inital starting window size and it builts upon it.
+#               - for sliding window this is the size of train and slide by 1 (the step size)
 
-RN = CV$Date
+CV_Results = Benchmark(DF, start_width = 200, expand = F)
+
+RN = CV_Results$Date
 
 library(dygraphs)
 library(xts)
-Plot = CV
+Plot = CV_Results
 rownames(Plot) = RN
 Plot$Date = NULL
 
