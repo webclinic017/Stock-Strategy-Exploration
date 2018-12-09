8261eb7db8fe413f6cbfee6343904673caaf69cd
AbramYorde <33256566+AbramYorde@users.noreply.github.com>
2018-12-08 22:51:53 -0500

    Implemented Feature Selection Output

--- Codes/Functions/Stepwise_Log.R
+++ Codes/Functions/Stepwise_Log.R
@@ -16,9 +16,9 @@ Stepwise_Log = function(DF){
   # DF_mod = na.omit(DF_Train) %>%
   #   dplyr::select(-c(Date,Stock,Adj_Smooth,Max,Days,PR))
   # ################## End Sample Data ###########
-  
+  DF = na.omit(DF)
   Weight_Vector = abs(DF$PR)
-  DF_mod = na.omit(DF) %>%
+  DF_mod = DF %>%
     dplyr::select(-c(Date,Stock,Adj_Smooth,Max,Days,PR))
   
   # NZV, Highly Correlated, Linear Combo Reduction
@@ -33,11 +33,12 @@ Stepwise_Log = function(DF){
   # Running Boruta 1st to Limit Columns
   Stage_1 = Boruta(Buy~.,
                    DF_mod,
-                   doTrace = 2)
+                   doTrace = 1,
+                   maxRuns = 25)
   Imp = as.data.frame(Stage_1$finalDecision)
   Keep = rownames(Imp)[Imp$`Stage_1$finalDecision` == "Confirmed"]
   DF_mod = DF_mod %>%
-    select("Buy",Keep)
+    dplyr::select("Buy",Keep)
   
   # Importance Plot
   Inf_Func = function(x){sum(is.infinite(x)) == 0}
@@ -55,42 +56,44 @@ Stepwise_Log = function(DF){
                    family = binomial,
                    weights = Weight_Vector)
   
-  ## Summarize the final selected model
-  summary(model_base)
-  ## Make predictions
-  probabilities <- model_base %>% predict(DF_Test, type = "response")
-  probabilities.df = model_base %>%
-    predict(DF,
-            type = "response")
-  predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
-  predicted.classes.df <- ifelse(probabilities.df > 0.5, 1, 0)
-  DF$Prob = probabilities.df
-  DF = na.omit(DF)
-  DF_Save = DF %>%
-    dplyr::select(PR_1D,Adjusted,Buy,Prob,PR,Date)
-  save(DF_Save,
-       file = "C://users//aayorde//desktop//Step_Reg_Results.RDATA")
-  DF_Test$Pred = predicted.classes
-  DF_Test$Prob = probabilities
-  DF_Test$Correct = DF_Test$Pred == DF_Test$Buy
-  Check = DF_Test[DF_Test$Pred == 1,]
-  Cumulatie_Return = cumsum(Check$PR_1D)
-  plot(Cumulatie_Return)
-  abline(h = 0)
-  ## Model accuracy
-  Acc = mean(predicted.classes==DF_Test$Buy,na.rm=T)
-
-  DF_Test$x = seq(1,nrow(DF_Test))
-  ggplot(DF_Test,aes(Date,Buy)) +
-    geom_point(aes(color = Correct)) +
-    geom_point(aes(y = Prob,color = Correct)) +
-    labs(x = "",
-         y = "Probability & Actual\n",
-         title = "Amazon Test Set",
-         subtitle = paste("Stepwise Logistic Regression Accuracy\n% Reuturn",
-                          round(sum(DF_Test$PR_1D[DF_Test$Pred == 1]),2)),
-         caption = paste("Buy Sell Accuracy :",
-                         scales::percent(Acc)))
-  
-  return(model_step)
+  # ## Summarize the final selected model
+  # summary(model_base)
+  # ## Make predictions
+  # probabilities <- model_base %>% predict(DF_Test, type = "response")
+  # probabilities.df = model_base %>%
+  #   predict(DF,
+  #           type = "response")
+  # predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
+  # predicted.classes.df <- ifelse(probabilities.df > 0.5, 1, 0)
+  # DF$Prob = probabilities.df
+  # DF = na.omit(DF)
+  # DF_Save = DF %>%
+  #   dplyr::select(PR_1D,Adjusted,Buy,Prob,PR,Date)
+  # save(DF_Save,
+  #      file = "C://users//aayorde//desktop//Step_Reg_Results.RDATA")
+  # DF_Test$Pred = predicted.classes
+  # DF_Test$Prob = probabilities
+  # DF_Test$Correct = DF_Test$Pred == DF_Test$Buy
+  # Check = DF_Test[DF_Test$Pred == 1,]
+  # Cumulatie_Return = cumsum(Check$PR_1D)
+  # plot(Cumulatie_Return)
+  # abline(h = 0)
+  # ## Model accuracy
+  # Acc = mean(predicted.classes==DF_Test$Buy,na.rm=T)
+  # 
+  # DF_Test$x = seq(1,nrow(DF_Test))
+  # ggplot(DF_Test,aes(Date,Buy)) +
+  #   geom_point(aes(color = Correct)) +
+  #   geom_point(aes(y = Prob,color = Correct)) +
+  #   labs(x = "",
+  #        y = "Probability & Actual\n",
+  #        title = "Amazon Test Set",
+  #        subtitle = paste("Stepwise Logistic Regression Accuracy\n% Reuturn",
+  #                         round(sum(DF_Test$PR_1D[DF_Test$Pred == 1]),2)),
+  #        caption = paste("Buy Sell Accuracy :",
+  #                        scales::percent(Acc)))
+  Names = colnames(DF_mod)
+  Output = list(Names = Names,
+                Mod = model_base)
+  return(Output)
 }
