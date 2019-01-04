d0099963959a7ca88ccba75c243a68b3bfd00177
AbramYorde <33256566+AbramYorde@users.noreply.github.com>
2019-01-04 16:05:40 -0500

    Fixed training set error when no data after rolling scale.

--- Codes/Functions/Training_Set_Function.R
+++ Codes/Functions/Training_Set_Function.R
@@ -27,61 +27,67 @@ Training_Set_Function = function(Combined_Results){
         na.omit() %>%
         as.data.frame()
       
-      TMP = DF %>%
-        mutate(Buy = 0)
-      Min = TMP$Adjust_TMP[1]
-      for(k in 2:(nrow(TMP)-1)){
-        Stance = TMP$Buy[k-1]
-        Delta = (TMP$Adjust_TMP[k] - TMP$Adjust_TMP[k-1])/TMP$Adjust_TMP[k-1]
+      if(nrow(DF) >= 365){
         
-        # Not Currently Holding and Price Reduction
-        if(Stance == 0 & Delta < 0){
-          if(TMP$Adjust_TMP[k] < Min){
-            Min = TMP$Adjust_TMP[k]
-          }
-          TMP$Buy[k] = 0
-        }
-        # Not Currently Holding and Price Increase
-        if(Stance == 0 & Delta > 0){
-          if((TMP$Adjust_TMP[k]-Min)/Min >= 0.05){
-            TMP$Buy[k] = 1
-            Purchase = TMP$Adjust_TMP[k]
-            Max = Purchase
-            counter = 0
-          }else{
+        TMP = DF %>%
+          mutate(Buy = 0)
+        Min = TMP$Adjust_TMP[1]
+        for(k in 2:(nrow(TMP)-1)){
+          Stance = TMP$Buy[k-1]
+          Delta = (TMP$Adjust_TMP[k] - TMP$Adjust_TMP[k-1])/TMP$Adjust_TMP[k-1]
+          
+          # Not Currently Holding and Price Reduction
+          if(Stance == 0 & Delta < 0){
+            if(TMP$Adjust_TMP[k] < Min){
+              Min = TMP$Adjust_TMP[k]
+            }
             TMP$Buy[k] = 0
           }
-        }
-        # Currently Holding and Price Reduction
-        if(Stance == 1 & Delta <= 0){
-          Diff_Purc = (TMP$Adjust_TMP[k] - Purchase)/Purchase
-          Diff_Max = (TMP$Adjust_TMP[k] - Max)/Max
-          if(Diff_Max <= -0.10 | Diff_Purc <= -0.05){
-            TMP$Buy[k] = 0
-            Min = TMP$Adjust_TMP[k]
-          }else{
+          # Not Currently Holding and Price Increase
+          if(Stance == 0 & Delta > 0){
+            if((TMP$Adjust_TMP[k]-Min)/Min >= 0.05){
+              TMP$Buy[k] = 1
+              Purchase = TMP$Adjust_TMP[k]
+              Max = Purchase
+              counter = 0
+            }else{
+              TMP$Buy[k] = 0
+            }
+          }
+          # Currently Holding and Price Reduction
+          if(Stance == 1 & Delta <= 0){
+            Diff_Purc = (TMP$Adjust_TMP[k] - Purchase)/Purchase
+            Diff_Max = (TMP$Adjust_TMP[k] - Max)/Max
+            if(Diff_Max <= -0.10 | Diff_Purc <= -0.05){
+              TMP$Buy[k] = 0
+              Min = TMP$Adjust_TMP[k]
+            }else{
+              TMP$Buy[k] = 1
+            }
+          }
+          # Currently Holding and Price Increase
+          if(Stance == 1 & Delta > 0){
+            if(TMP$Adjust_TMP[k] > Max){
+              Max = TMP$Adjust_TMP[k]
+            }
             TMP$Buy[k] = 1
           }
         }
-        # Currently Holding and Price Increase
-        if(Stance == 1 & Delta > 0){
-          if(TMP$Adjust_TMP[k] > Max){
-            Max = TMP$Adjust_TMP[k]
-          }
-          TMP$Buy[k] = 1
-        }
-      }
-    
-    TMP = TMP %>%
-      mutate(Indicator = sequence(rle(Buy)$lengths),
-             Max = ifelse(lead(Indicator) == 1,Indicator + 1,NA)) %>%
-      na.locf(fromLast = T,na.rm = F) %>%
-      mutate(W = 1/Indicator)
-    TMP$Buy[TMP$Max < 10] = 0
+        
+        TMP = TMP %>%
+          mutate(Indicator = sequence(rle(Buy)$lengths),
+                 Max = ifelse(lead(Indicator) == 1,Indicator + 1,NA)) %>%
+          na.locf(fromLast = T,na.rm = F) %>%
+          mutate(W = 1/Indicator)
+        TMP$Buy[TMP$Max < 10] = 0
+        
     
     # ggplot(TMP,aes(x = Date,y = Adjust_TMP)) +
     #   geom_point(aes(color = factor(Buy)))
 
+      }else{
+        TMP = NA
+      }
       ## Finding Optimal Stat Windows
       ## Removing Date Information
       Smooth_Data =  try(TMP %>%
