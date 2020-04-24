93856d54c8ee2612631a1bc2514211bcd000ad2a
AbramYorde <33256566+AbramYorde@users.noreply.github.com>
2020-04-23 22:09:42 -0400

    continued to chip away at python understanding

--- Python/Alpaca_Integration_Testing.py
+++ Python/Alpaca_Integration_Testing.py
@@ -3,6 +3,12 @@ import alpaca_trade_api as tradeapi
 import pandas as pd
 import numpy as np
 import os
+from tqdm import tqdm
+
+## Input Parameters
+min_price = 15
+save_loc = 'C://Users//aayorde//documents//github//stock-strategy-exploration/Data/'
+historical_amount = 90
 
 ## Live or Paper Execution
 Live = False
@@ -23,27 +29,43 @@ else:
     os.environ['APCA_API_SECRET_KEY'] = Paper_Secret
     os.environ['APCA_API_BASE_URL'] ='https://paper-api.alpaca.markets'
 
+os.environ['APCA_RETRY_WAIT'] = 6
+
+
 ## Initializing API
-api = tradeapi.REST()
+api = tradeapi.REST(api_version='v2')
 
 
 ## Pulling Historical Data
 assets = api.list_assets(status = 'active')
 good_assets = [a.symbol for a in assets if 
-    a.marginable and a.shortable and a.tradable and a.marginable and a.easy_to_borrow]
+    a.marginable and 
+    a.shortable and 
+    a.tradable and 
+    a.marginable and 
+    a.easy_to_borrow]
 
-bar_data = pd.DataFrame(columns = ('s','o','h','l','c','t','v'))
-for i in range(len(good_assets)):
+## Loading Existing File If Exists
+if os.path.exists(save_loc + "Historical_Data.pickle"):
+    bar_data = pd.read_pickle(save_loc + "Historical_Data.pickle")
+    after = max(bar_data.t).isoformat()
+else:
+    bar_data = pd.DataFrame(columns = ('s','o','h','l','c','t','v'))
+    after = (pd.Timestamp.today(tz = 'America/New_York') - pd.Timedelta(days = historical_amount)).isoformat()
+
+## Pulling New Data
+for i in tqdm(range(len(good_assets))):
     s = good_assets[i]
-    bars = api.get_barset(s,'day',limit = 5)
-    tmp = [np.array([s,a.o,a.h,a.l,a.c,a.t,a.v]) for a in bars[s]]
+    bars = api.get_barset(s,'day',limit = historical_amount,after = after)
+    tmp = [[s,a.o,a.h,a.l,a.c,a.t,a.v] for a in bars[s]]
+    if tmp[0][1] < min_price:
+        next
     bar_data = bar_data.append(pd.DataFrame(tmp,columns = bar_data.columns))
 
+## Saving / Loading Historical Data
+bar_data.to_pickle(save_loc + "Historical_Data.pickle")
+bar_data = pd.read_pickle(save_loc + "Historical_Data.pickle")
 
-
-
-
+## Loading Account Information
 account = api.get_account()
-
-
 print(account)
